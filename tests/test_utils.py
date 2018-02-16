3f3565424cbe4f61b91a8a0ce518c8f7f901069f
Jens Finnäs <jens.finnas@gmail.com>
2018-02-16 17:14:17 +0100

    ...test that also

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -22,6 +22,33 @@ class TestUtils(TestCase):
 
     def test_iter_options(self):
         #TBD
+        select_elem = """
+        <select name="psAr" onchange="reload(this.form);">\n
+        <option selected="" value="2016">2016/17\n
+        <option value="2015">2015/16\n
+        <option value="2014">2014/15\n
+        <option value="2013">2013/14\n
+        <option value="2012">2012/13\n
+        <option value="2011">2011/12\n
+        <option value="2010">2010/11\n
+        <option value="2009">2009/10\n
+        <option value="2008">2008/09\n
+        <option value="2007">2007/08\n
+        <option value="2006">2006/07\n
+        <option value="2005">2005/06\n
+        <option value="2004">2004/05\n
+        <option value="2003">2003/04\n
+        <option value="2002">2002/03\n
+        <option value="2001">2001/02\n
+        <option value="2000">2000/01\n
+        <option value="1999">1999/00\n
+        <option value="1998">1998/99\n
+        <option value="1997">1997/98\n
+        </option></option></option></option></option></option></option></option></option></option></option></option></option></option></option></option></option></option></option></option></select>
+        """
+        options = [x for x in iter_options(select_elem)]
+        assert len(options) == 20
+
         pass
 
     def test_parse_value(self):
